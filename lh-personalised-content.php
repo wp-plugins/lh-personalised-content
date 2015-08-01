d1fc8441bc1b2be46e7d4df3fc1f96b062cc94cf
shawfactor <shawfactor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 04:46:34 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/lh-personalised-content/trunk@1210738 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lh-personalised-content.php
@@ -0,0 +1,206 @@
+<?php
+/*
+Plugin Name: LH Personalised Content
+Version: 1.1
+Plugin URI: http://lhero.org/plugins/lh-personalised-content/
+Description: Creates a shortcodes for personalised content that can be used on your website of your WordPress emails
+Author: Peter Shaw
+Author URI: http://shawfactor.com
+*/
+
+class LH_personalised_content_plugin {
+
+
+function check_user(){
+
+if ($GLOBALS['lh_personalised_user']){
+
+$current_user = $GLOBALS['lh_personalised_user'];
+
+} else {
+
+$current_user = wp_get_current_user();
+
+
+}
+
+if ($current_user->ID){
+
+return $current_user;
+
+} else {
+
+
+return false;
+
+
+}
+
+
+
+
+}
+
+function strReplaceAssoc(array $replace, $subject) {
+   return str_replace(array_keys($replace), array_values($replace), $subject);   
+} 
+
+function return_sender_user($to){
+
+
+$emailArray = explode(',', str_replace(' ', '', $to));
+
+$result = count($emailArray);
+
+if ($result > 1){
+
+// There is more than one recipient
+
+return false;
+
+} else {
+
+if ($user = get_user_by('email', $emailArray[0])){
+
+
+// The recipient is in the system
+
+return $user;
+
+
+} else {
+
+
+// The recipient is not in the system
+
+return false;
+
+
+}
+
+}
+
+
+}
+
+
+function wp_mail_filter( $args ) {
+
+$subject = $args['subject'];
+
+
+if ((has_shortcode( $args['message'], 'lh_personalised_content' )) or (has_shortcode( $args['subject'], 'lh_personalised_content' )) ) {
+
+
+if ($user = $this->return_sender_user($args['to'])){
+
+$GLOBALS['lh_personalised_user'] = $user;
+
+
+} else {
+
+
+$GLOBALS['lh_personalised_user'] = "none";
+
+
+}
+
+
+$args['subject'] = do_shortcode($args['subject']);
+
+
+$args['message'] = do_shortcode($args['message']);
+
+
+
+
+}
+
+
+	$new_wp_mail = array(
+		'to'          => $args['to'],
+		'subject'     => $args['subject'],
+		'message'     => $args['message'],
+		'headers'     => $args['headers'],
+		'attachments' => $args['attachments'],
+	);
+	
+	return $new_wp_mail;
+}
+
+function lh_personalised_content_output($atts,$content = null) {
+
+    // define attributes and their defaults
+    extract( shortcode_atts( array (
+        'role' => '', //Allow matching of roles, this functionality will be added later
+        'loggedout' => ''
+    ), $atts ) );
+
+
+if ($current_user = $this->check_user()){
+
+
+$add = $current_user->data;
+
+//print_r($current_user);
+
+
+foreach ($add as $key => $value) {
+
+$newkey = "%".$key."%";
+
+$newarray[$newkey] = $value;
+
+}
+
+$newarray['%first_name%'] = get_user_meta( $add->ID, 'first_name', true );
+
+$newarray['%last_name%'] = get_user_meta( $add->ID, 'last_name', true );
+
+$newarray['%description%'] = get_user_meta($add->ID, 'description', true);
+
+
+$content = $this->strReplaceAssoc($newarray, $content);
+
+
+
+} else {
+
+
+$content = $loggedout;
+
+
+}
+
+return $content;
+
+}
+
+
+
+function register_shortcodes(){
+
+add_shortcode('lh_personalised_content', array($this,"lh_personalised_content_output"));
+
+}
+
+
+
+
+
+function __construct() {
+
+add_filter( 'wp_mail', array($this,"wp_mail_filter"));
+
+add_action( 'init', array($this,"register_shortcodes"));
+
+
+}
+
+
+}
+
+$lh_personalised_content = new LH_personalised_content_plugin();
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== LH Personalised Content ===
+Contributors: shawfactor
+Tags: personalise, personalize, emails, user, name, first name, sender email, sender name, sender
+Requires: 3.0
+Tested up to: 4.2
+Version: 1.1
+Stable tag: Trunk
+
+This plugin allows one to personalise wordpress emails, or content for a logged in user.
+
+== Description ==
+This plugin allows one to personalise content for a logged in user or emails for a email recipeient that is in the user database.  It uses shortcode type functionality in any email, page, post, or custpm post type. It extract stored details of the viewer or reader. A default text can be specified for situations where there are no stored details.
+
+This can allow you to offer a more customised service for example with a more helpful unsubscribe link.  Or you could create your own "profile" page for logged in users only.
+
+Check out [our documentation][docs] for more information. 
+
+All tickets for the project are being tracked on [GitHub][].
+
+[docs]: https://github.com/shawfactor/lh-personalised-content
+[GitHub]: https://github.com/shawfactor/lh-login-page
+
+
+== Changelog ==
+= 1.0 =
+
+* Launch of the plugin
+
+
+= 1.1 =
+
+* Enhanced documentation
+
+
+== Installation ==
+
+1. In the wordpress admin plugin section of your site, click "Add New" or download and Unzip the file into your wordpress plugins folder.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the shortcode to your posts, pages, cpt's or emails as described in the how to use section
+
+= How to use  =
+
+Simplest: Put [lh_personalised_content]Content[/lh_personalised_content] in your page, post, or email template.  eg: If you want to welcome only logged in users you could put [lh_personalised_content]Welcome logged in users[/lh_personalised_content]
+
+This plugin also allows you to create personalised templates for logged in users or email recipients. By the use of placeholders based on wordpress user data. The following data is supported: %user_login% - the users username, %user_email% - the users email, %first_name% - the users first name, %last_name% - the users last name, %display_name% - the users display name, %ID% - the users ID, %description% - the users bio or description.
+
+More attributes will be added in time.
+
+The shortcode also has a loggedout attribute. This is only displayed to logged out users.
+
+Examples:
+
+*  [lh_personalised_content]%display_name%[/lh_personalised_content]     will display display name to logged in users and nothing to logged out users
+*  [lh_personalised_content loggedout="Hello guest"]%first_name%[/lh_personalised_content] will display the users first name to logged in users and "hello guest" to logged out users
+
+
